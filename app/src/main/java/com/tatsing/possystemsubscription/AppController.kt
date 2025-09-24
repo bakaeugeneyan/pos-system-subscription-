fe9eee5837f25d95687d068b9e6f3a81a1acab64
bakaeugeneyan <eugene.veladiez@gmail.com>
2025-09-24 11:53:30 +0800

    setup dependecies

---
+++ app/src/main/java/com/tatsing/possystemsubscription/AppController.kt
@@ -0,0 +1,12 @@
+package com.tatsing.possystemsubscription
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppController:Application() {
+	override fun onCreate() {
+		super.onCreate()
+//		AppAuth.initialize(applicationContext)
+	}
+}
